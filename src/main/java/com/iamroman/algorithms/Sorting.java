5fc2dbd0eecd93cb7957a19ce559641ba7b7e5b8
Roman Torres <romantorrex@gmail.com>
2020-04-25 09:30:39 -0500

    Adds implementation of insertion sort

---
+++ src/main/java/com/iamroman/algorithms/Sorting.java
@@ -0,0 +1,18 @@
+package com.iamroman.algorithms;
+
+public abstract class Sorting {
+
+	public static void insertionSort(int[] data) {
+		for (int i = 1; i < data.length; i++) {
+			int position = i;
+			int value = data[i];
+
+			while (position > 0 && data[position - 1] > value ) {
+				data[position] = data[position-1];
+				position--;	
+			}
+
+			data[position] = value;
+		}
+	}
+}
