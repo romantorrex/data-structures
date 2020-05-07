f3b529adf32e093a70a62ab437061162edf9343d
Roman Torres <romantorrex@gmail.com>
2020-05-07 07:47:23 -0500

    Adds implementation of bubble sort

--- src/main/java/com/iamroman/algorithms/Sorting.java
+++ src/main/java/com/iamroman/algorithms/Sorting.java
@@ -15,4 +15,24 @@ public abstract class Sorting {
       data[position] = value;
     }
   }
+
+  public static void bubbleSort(int[] array) {
+    boolean sorted = false;
+
+    while (!sorted) {
+      sorted = true;
+      for (int i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+          swap(array, i, i + 1);
+          sorted = false;
+        }
+      }
+    }
+  }
+
+  private static void swap(int[] array, int i, int j) {
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 }
--- src/test/java/com/iamroman/algorithms/SortingTest.java
+++ src/test/java/com/iamroman/algorithms/SortingTest.java
@@ -7,9 +7,10 @@ import org.junit.Test;
 
 public class SortingTest {
 
-  @Test
+  private static String SORTED_ARRAY_STR = "[0, 2, 5, 6, 7]";
+
   public void insertionSort() {
-    int[] numbers = {6, 2, 0, 7, 5};
+    int[] numbers = {5, 2, 7, 6, 0};
     Sorting.insertionSort(numbers);
 
     assertThat(Arrays.toString(numbers)).isEqualTo("[0, 2, 5, 6, 7]");
@@ -18,5 +19,19 @@ public class SortingTest {
     Sorting.insertionSort(numbers);
 
     assertThat(Arrays.toString(numbers)).isEqualTo("[0, 1, 2, 3, 4, 5]");
+  }
+
+  @Test
+  public void bubbleSort() {
+    int[] numbers = {5, 2, 7, 6, 0};
+
+    Sorting.bubbleSort(numbers);
+
+    assertThat(Arrays.toString(numbers)).isEqualTo(SORTED_ARRAY_STR);
+
+    int[] worstCase = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
+    Sorting.bubbleSort(worstCase);
+    assertThat(Arrays.toString(worstCase)).isEqualTo("[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]");
   }
 }
