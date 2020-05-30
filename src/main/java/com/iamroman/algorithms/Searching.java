da5d7a14dc243ba9348b51a1195849fe0e8a65fc
Roman Torres <romantorrex@gmail.com>
2020-05-30 13:16:48 -0500

    Applies proper formatting

--- src/main/java/com/iamroman/algorithms/Searching.java
+++ src/main/java/com/iamroman/algorithms/Searching.java
@@ -2,21 +2,21 @@ package com.iamroman.algorithms;
 
 public abstract class Searching {
 
-	public static int binarySearch(int[] array, int x) {
-		int left = 0;
-		int right = array.length - 1;
+  public static int binarySearch(int[] array, int x) {
+    int left = 0;
+    int right = array.length - 1;
 
-		while (left <= right) {
-		int middle = (left + right) / 2;
-			if (array[middle] == x) {
-				return middle;
-			} else if(x < array[middle]) {
-				right = middle - 1;
-			} else {
-				left = middle + 1;
-			}
-		}
+    while (left <= right) {
+      int middle = (left + right) / 2;
+      if (array[middle] == x) {
+        return middle;
+      } else if (x < array[middle]) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+    }
 
-		return -1;
-	}
+    return -1;
+  }
 }
