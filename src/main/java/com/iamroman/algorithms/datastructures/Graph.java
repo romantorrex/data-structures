3d2981a654057c280420632b81902fc3148653fc
Roman Torres <romantorrex@gmail.com>
2020-06-27 19:42:42 -0500

    Adds methods to directed graph implementation

--- src/main/java/com/iamroman/algorithms/datastructures/Graph.java
+++ src/main/java/com/iamroman/algorithms/datastructures/Graph.java
@@ -1,29 +1,66 @@
 package com.iamroman.algorithms.datastructures;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 /** A directed graph implementation using an adjacency list. */
 public class Graph {
-  private final Map<String, Set<Node>> adjacencies = new HashMap<>();
+  private final Map<String, List<Node>> adjacencyMap = new HashMap<>();
 
   public void addNode(String label) {
-    adjacencies.putIfAbsent(label, new HashSet<>());
+    adjacencyMap.putIfAbsent(label, new ArrayList<>());
   }
 
   public void addEdge(String from, String to) {
-    if (adjacencies.containsKey(from)) {
+    if (!adjacencyMap.containsKey(from)) {
       throw new IllegalArgumentException(
           String.format("Node %s does not exist in this graph.", from));
     }
 
-    if (!adjacencies.containsKey(to)) {
-      throw new IllegalArgumentException(String.format("Node %s does not exist in this graph", to));
+    if (!adjacencyMap.containsKey(to)) {
+      throw new IllegalArgumentException(
+          String.format("Node %s does not exist in this graph.", to));
     }
 
-    adjacencies.get(from).add(new Node(to));
+    adjacencyMap.get(from).add(new Node(to));
+  }
+
+  public void removeNode(String label) {
+    if (!adjacencyMap.containsKey(label)) {
+      throw new IllegalArgumentException(
+          String.format("Node %s does not exist in this graph.", label));
+    }
+
+    var node = new Node(label);
+    adjacencyMap.values().forEach(targets -> targets.remove(node));
+    adjacencyMap.remove(label);
+  }
+
+  public void removeEdge(String from, String to) {
+    if (!adjacencyMap.containsKey(from)) {
+      throw new IllegalArgumentException(
+          String.format("Node %s does not exist in this graph.", from));
+    }
+    if (!adjacencyMap.containsKey(to)) {
+      throw new IllegalArgumentException(
+          String.format("Node %s does not exist in this graph.", to));
+    }
+
+    var node = new Node(to);
+    adjacencyMap.get(from).remove(node);
+  }
+
+  public String nodesAsString() {
+    return adjacencyMap.keySet().stream()
+        .map(nodeLabel -> String.format("(%s)", nodeLabel))
+        .collect(Collectors.joining(", "));
+  }
+
+  public String edgesAsString() {
+    return adjacencyMap.entrySet().stream()
+        .map(entry -> String.format("(%s) -> %s", entry.getKey(), entry.getValue()))
+        .collect(Collectors.joining(", "));
   }
 
   private class Node {
@@ -31,6 +68,24 @@ public class Graph {
 
     Node(String label) {
       this.label = label;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("(%s)", label);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Node node = (Node) o;
+      return Objects.equals(label, node.label);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(label);
     }
   }
 }
---
+++ src/test/java/com/iamroman/algorithms/datastructures/GraphTest.java
@@ -0,0 +1,73 @@
+package com.iamroman.algorithms.datastructures;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import org.junit.Test;
+
+public class GraphTest {
+  private Graph graph = new Graph();
+
+  @Test
+  public void addNode() {
+    graph.addNode("a");
+    graph.addNode("b");
+    graph.addNode("c");
+
+    assertThat(graph.nodesAsString()).isEqualTo("(a), (b), (c)");
+  }
+
+  @Test
+  public void addNode_alreadyInTheGraph() {
+    graph.addNode("a");
+    graph.addNode("a");
+
+    assertThat(graph.nodesAsString()).isEqualTo("(a)");
+  }
+
+  @Test
+  public void removeNode() {
+    graph.addNode("a");
+    graph.addNode("b");
+    graph.addNode("c");
+
+    graph.removeNode("b");
+
+    assertThat(graph.nodesAsString()).isEqualTo("(a), (c)");
+  }
+
+  @Test
+  public void removeNode_whenLabelDoesNotExist_throwsException() {
+    Throwable throwable = catchThrowable(() -> graph.removeNode("c"));
+
+    assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+    assertThat(throwable).hasMessage("Node c does not exist in this graph.");
+  }
+
+  @Test
+  public void addEdge() {
+    graph.addNode("a");
+    graph.addNode("b");
+    graph.addNode("c");
+
+    graph.addEdge("a", "b");
+    graph.addEdge("a", "c");
+    graph.addEdge("b", "c");
+
+    assertThat(graph.edgesAsString()).isEqualTo("(a) -> [(b), (c)], (b) -> [(c)], (c) -> []");
+  }
+
+  @Test
+  public void removeEdge() {
+    graph.addNode("a");
+    graph.addNode("b");
+    graph.addNode("c");
+    graph.addEdge("a", "b");
+    graph.addEdge("a", "c");
+    graph.addEdge("b", "c");
+
+    graph.removeEdge("a", "b");
+
+    assertThat(graph.edgesAsString()).isEqualTo("(a) -> [(c)], (b) -> [(c)], (c) -> []");
+  }
+}
