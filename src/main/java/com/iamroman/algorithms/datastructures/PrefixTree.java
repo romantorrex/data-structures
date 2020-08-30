43b4f27ca1cd042bfa9c82f0d959071e11718af5
Roman Torres <roman@Mac-mini-de-Roman.local>
2020-08-29 21:39:40 -0500

    Add an implementation of a prefix tree

---
+++ src/main/java/com/iamroman/algorithms/datastructures/PrefixTree.java
@@ -0,0 +1,69 @@
+package com.iamroman.algorithms.datastructues;
+
+// TDOD: Add unit tests.
+public class PrefixTree {
+  private Node root = new Node(' ');
+
+  public void insert(String word) {
+    Node current = root;
+
+    for (char c : word.toCharArray()) {
+      Node node = current.getChildWithValue(c);
+      if (node == null) {
+        node = new Node(c);
+      }
+      current.addChild(node);
+      current = node;
+    }
+
+    current.setEndOfWord(true);
+  }
+
+  private class Node {
+    private char value;
+    private final Node[] children = new Node[26];
+    private boolean endOfWord;
+
+    public Node(char value) {
+      this.value = value;
+    }
+
+    public char getValue() {
+      return value;
+    }
+
+    public Node[] getChildren() {
+      return children;
+    }
+
+    public boolean isEndOfWord() {
+      return endOfWord;
+    }
+
+    public void setEndOfWord(boolean endOfWord) {
+      this.endOfWord = endOfWord;
+    }
+
+    public boolean hasChildWithVallue(char value) {
+
+      return children[indexOf(value)] != null;
+    }
+
+    public Node getChildWithValue(char value) {
+      return children[indexOf(value)];
+    }
+
+    public void addChild(Node child) {
+      children[indexOf(child.getValue())] = child;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("(%s)", value);
+    }
+
+    private int indexOf(char letter) {
+      return letter - 'a';
+    }
+  }
+}
