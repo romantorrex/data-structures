80be5df2838dfab11adfc08be9e302ec04e181ee
Roman Torres <romantorrex@gmail.com>
2020-09-14 21:16:22 -0500

    Implements shortest distance algorithm

--- src/main/java/com/iamroman/algorithms/datastructures/WeightedGraph.java
+++ src/main/java/com/iamroman/algorithms/datastructures/WeightedGraph.java
@@ -1,8 +1,8 @@
 package com.iamroman.algorithms.datastructures;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 /** Undirected graph with weights in its edges */
 public class WeightedGraph {
@@ -27,9 +27,41 @@ public class WeightedGraph {
     toNode.addEdge(fromNode, weight);
   }
 
+  // TODO: Add unit tests
+  public int shortestDistance(String from, String to) {
+    Node fromNode = nodes.get(from);
+    HashMap<String, Integer> distances = new HashMap<>();
+    HashSet<Node> visited = new HashSet<>();
+    PriorityQueue<NodeEntry> queue =
+        new PriorityQueue<>(Comparator.comparingInt(entry -> entry.priority));
+
+    for (Node node : nodes.values()) {
+      distances.put(node.label, Integer.MAX_VALUE);
+    }
+    distances.replace(from, 0);
+
+    queue.add(new NodeEntry(fromNode, 0));
+    while (!queue.isEmpty()) {
+      Node currentNode = queue.remove().node;
+
+      for (Edge edge : currentNode.edges) {
+        if (visited.contains(edge.to)) {
+          continue;
+        }
+
+        int newDistance = distances.get(currentNode.label) + edge.weight;
+        if (newDistance < distances.get(edge.to.label)) {
+          distances.replace(edge.to.label, newDistance);
+          queue.add(new NodeEntry(edge.to, newDistance));
+        }
+      }
+    }
+    return distances.get(to);
+  }
+
   private class Node {
     private String label;
-    private List<Edge> edges;
+    private List<Edge> edges = new ArrayList<>();
 
     public Node(String label) {
       this.label = label;
@@ -49,6 +81,16 @@ public class WeightedGraph {
       this.from = from;
       this.to = to;
       this.weight = weight;
+    }
+  }
+
+  private class NodeEntry {
+    private Node node;
+    private int priority;
+
+    public NodeEntry(Node node, int priority) {
+      this.node = node;
+      this.priority = priority;
     }
   }
 }
